d0c29871a72b67dd617b10476d49843963bd58ee
paudelrajan007 <rajan11paudel@gmail.com>
2024-10-05 22:30:32 +0530

    hi

---
+++ chapter 2/deletearray.c
@@ -0,0 +1,34 @@
+//delete
+#include <stdio.h>
+
+int main() {
+    char a[50];
+    char word;
+    int index,arraylength,i;
+    
+    printf("Enter array length :");
+    scanf("%d", &arraylength);
+    
+    printf("Enter elements :");
+    for(i=0;i<arraylength;i++){
+    scanf(" %c",&a[i]);
+    }
+    
+    printf("Enter index where u want to delete word :");
+    scanf("%d",&index);
+    
+    if(index<0||index>arraylength){
+        printf("You reach maximum \"Thank you\"");
+    }else{
+        for(i=index;i<arraylength-1;i++){
+            a[i]=a[i+1];
+        }
+        
+        arraylength--;
+        
+    }for(i=0;i<arraylength;i++){
+        printf(" %c\n",a[i]);
+    }
+
+    return 0;
+}
