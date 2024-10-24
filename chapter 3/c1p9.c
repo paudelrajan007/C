589ce3cf5bab8f666dba4f8995384b34a0632457
paudelrajan007 <rajan11paudel@gmail.com>
2024-10-24 18:50:11 +0530

    hi

--- chapter 3/c1p9.c
+++ chapter 3/c1p9.c
@@ -1,11 +1,72 @@
 #include<stdio.h>
-void main(){
-    int a[100],number,index,length,i;
-    printf("Enter length of array :\t");
-    if(i<0||i>100){
-        printf("Thank you ");
-
-    }else{
-        scanf("%d",&length)
+void main (){
+int a[100],number,index,i,choice,arraylength;
+printf("Enter array length max (100) min (0): \t");
+scanf("%i",&arraylength);
+if(arraylength<0||arraylength>100){
+    printf("You reach max(100) and min(0) :\t");
+    printf("\"Thank You \"!\t");
+}else{
+        printf("Enter array elements :\t");
+    for(i=0;i<arraylength;i++){
+        scanf("%i",&a[i]);
     }
+    while(1){
+        printf("Choose an operation :\n");
+        printf("1: Insult an elements \n");
+        printf("2: Delete an elements \n");
+        printf("3: Display an elements \n");
+        printf("4: Exit \n");
+        printf("Enter your choice :\t");
+        scanf("%i",&choice);
+    switch (choice){
+        case 1:
+        printf("Enter a number which you want to insult :\t");
+        scanf("%i",&number);
+        printf("Enter index where you want to insult :\t");
+        scanf("%i",&index);
+        if(index<0||index>arraylength){
+            printf("Invalid index \"Thank You\"");
+        }else{
+            for(i=arraylength;i>index;i--){
+                a[i]=a[i-1];
+                
+            }
+            a[index]=number;
+            arraylength++;
+            printf("Elements insult successfully\n");
+            break;
+        }
+        case 2:
+        printf("Enter index (0 to %i):\t",arraylength);
+        scanf("%i",&index);
+        if(index<0||index>arraylength){
+            printf("Invalide index");
+        }else{
+            for(i=index;i<arraylength-1;i++){
+                a[i]=a[i+1];
+            }arraylength--;
+            printf("Element deleted ");
+            break;
+        }
+        case 3:
+        printf("Current array elements :\t");
+        for(i=0;i<arraylength;i++){
+            printf("%i",a[i]);
+        }
+        printf("\n");
+        break;
+    case 4:
+    printf("Exit the program \"Thank You\" ");
+    break;
+default:
+printf("Invide choice:\t");
+    }
+    }
+    
 }
+
+}
+
+   
+   
